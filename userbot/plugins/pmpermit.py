aca7da47fa157bf9d2f0debf16084348c8f7e61c
Restyled.io <commits@restyled.io>
2020-10-14 06:01:03 +0000

    Restyled by autopep8

--- userbot/plugins/pmpermit.py
+++ userbot/plugins/pmpermit.py
@@ -26,7 +26,8 @@ DEFAULTUSER = (str(ALIVE_NAME)
                if ALIVE_NAME else "Set ALIVE_NAME in config vars in Heroku")
 CUSTOM_MIDDLE_PMP = (str(CUSTOM_PMPERMIT)
                      if CUSTOM_PMPERMIT else "Protection By Friday 🇮🇳")
-USER_BOT_WARN_ZERO = ("You Have Attempted To Spam Masters Inbox So Inorder To Avoid Over Spam , You Have Been Blocked By Userbot")
+USER_BOT_WARN_ZERO = (
+    "You Have Attempted To Spam Masters Inbox So Inorder To Avoid Over Spam , You Have Been Blocked By Userbot")
 
 botisnoob = Var.TG_BOT_USER_NAME_BF_HER
 USER_BOT_NO_WARN = (
