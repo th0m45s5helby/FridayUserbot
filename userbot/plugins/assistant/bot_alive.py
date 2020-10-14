01230fe176a51e996ceafebd6cf97256e84b2c7c
Restyled.io <commits@restyled.io>
2020-10-14 05:38:12 +0000

    Restyled by reorder-python-imports

--- userbot/plugins/assistant/bot_alive.py
+++ userbot/plugins/assistant/bot_alive.py
@@ -7,20 +7,33 @@ from math import ceil
 
 import emoji
 from googletrans import Translator
-from telethon import Button, custom, events
+from telethon import Button
+from telethon import custom
+from telethon import events
 from telethon.tl.functions.photos import GetUserPhotosRequest
 from telethon.tl.functions.users import GetFullUserRequest
-from telethon.tl.types import Channel, Chat, User
-from telethon.utils import get_display_name, pack_bot_file_id
-from uniborg.util import admin_cmd, sudo_cmd
+from telethon.tl.types import Channel
+from telethon.tl.types import Chat
+from telethon.tl.types import User
+from telethon.utils import get_display_name
+from telethon.utils import pack_bot_file_id
+from uniborg.util import admin_cmd
+from uniborg.util import sudo_cmd
 
-from userbot import ALIVE_NAME, CMD_LIST, Lastupdate, bot
-from userbot.plugins import currentversion, inlinestats
-from userbot.plugins.sql_helper.botusers_sql import add_me_in_db, his_userid
-from userbot.plugins.sql_helper.idadder_sql import (add_usersid_in_db,
-                                                    get_all_users)
+from userbot import ALIVE_NAME
+from userbot import bot
+from userbot import CMD_LIST
+from userbot import Lastupdate
+from userbot.plugins import currentversion
+from userbot.plugins import inlinestats
+from userbot.plugins.sql_helper.botusers_sql import add_me_in_db
+from userbot.plugins.sql_helper.botusers_sql import his_userid
+from userbot.plugins.sql_helper.idadder_sql import add_usersid_in_db
+from userbot.plugins.sql_helper.idadder_sql import get_all_users
 from userbot.uniborgConfig import Config
-from userbot.utils import admin_cmd, edit_or_reply, sudo_cmd
+from userbot.utils import admin_cmd
+from userbot.utils import edit_or_reply
+from userbot.utils import sudo_cmd
 
 DEFAULTUSER = str(ALIVE_NAME) if ALIVE_NAME else "Unknown"
 PM_IMG = "https://telegra.ph/file/22535f8051a58af113586.jpg"
