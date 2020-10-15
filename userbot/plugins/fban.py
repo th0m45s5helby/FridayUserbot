18ef1ebdbba7fe7193facb26c1cb612cd7cc06af
StarkGang <StarkGangZ@gmail.com>
2020-10-15 15:14:50 +0530

    Create fban.py

---
+++ userbot/plugins/fban.py
@@ -0,0 +1,26 @@
+# MIDHUN K M
+# (C) BY MIDHUN
+FOR FRIDAY
+from userbot.utils import admin_cmd
+from userbot.plugins.sql_helper.fban_sql import add_fed_in_db, get_all_fed, remove_fed, already_added_fed
+
+
+
+
+
+
+@borg.on(admin_cmd("fadd ?(.*)"))
+async def addfed(event):
+    if event.fwd_from:
+        return
+    sedlyf = event.pattern_match.group(1)
+    if not already_added_fed(sedlyf):
+        add_fed_in_db(sedlyf)
+        await edit_or_reply( "`Added Fed Successfuly To List`")
+        await asyncio.sleep(3)
+        await event.delete()
+    elif already_added_fed(sedlyf):
+        await event.edit("`Fed is already is database!`")
+        await asyncio.sleep(3)
+        await event.delete()
+ 
