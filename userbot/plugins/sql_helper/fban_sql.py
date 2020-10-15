c0206811d5c7a027c3c171c6ce60fddfe45fbe8a
StarkGang <StarkGangZ@gmail.com>
2020-10-15 11:45:08 +0530

    Create fban_sql.py

---
+++ userbot/plugins/sql_helper/fban_sql.py
@@ -0,0 +1,37 @@
+from sqlalchemy import Column
+from sqlalchemy import Integer
+from sqlalchemy import String
+
+from . import BASE
+from . import SESSION
+
+
+class Fban(BASE):
+    __tablename__ = "fban"
+    fedids = Column(UnicodeText, primary_key=True)
+
+    def __init__(self, fedids):
+        self.fedids = fedids
+
+Fban.__table__.create(checkfirst=True)
+
+def add_fed_in_db(fedids):
+    fed_id = Fban(fedids)
+    SESSION.add(fed_id)
+    SESSION.commit()
+
+def get_all_fed():
+    fed_ids = SESSION.query(Fban).all()
+    SESSION.close()
+    return fed_ids
+
+def removefeds(fedids):
+    fednibba = SESSION.query(Fban).get(fedids)
+    if fednibba:
+        SESSION.delete(fednibba)
+        SESSION.commit()
+
+def get_all_feds():
+    stark = SESSION.query(Fban).all()
+    SESSION.close()
+    return stark
