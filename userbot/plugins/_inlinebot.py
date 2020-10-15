e872c4147220885a64bd894524232585dd83c0d4
StarkGang <StarkGangZ@gmail.com>
2020-10-15 12:22:42 +0530

    Update _inlinebot.py

--- userbot/plugins/_inlinebot.py
+++ userbot/plugins/_inlinebot.py
@@ -33,7 +33,7 @@ if Var.TG_BOT_USER_NAME_BF_HER is not None and tgbot is not None:
             )
         if event.query.user_id == bot.uid and query == "stats":
             result = builder.photo(
-                content=sed,
+                file=sed,
                 text=f"**Showing Stats For {DEFAULTUSER}'s Friday** \nNote --> Only Owner Can Check This \n(C) @FridayOT",
                 buttons=[
                     [custom.Button.inline("Show Stats ", data="terminator")],
